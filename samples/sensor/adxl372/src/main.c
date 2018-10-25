83e64feec779c6231f8d1eef462b9691353e3721
Michael Hennerich <michael.hennerich@analog.com>
2018-10-25 14:02:27 +0200

    samples: sensor: adxl372: Add option to test SENSOR_ATTR_WAKEUP_THRESH
    To enable wakeup mode define WAKEUP_THRESH with a in range value in m/s^2
    
    #define WAKEUP_THRESH 		(35 * SENSOR_G)
    
    Signed-off-by: Michael Hennerich <michael.hennerich@analog.com>

--- samples/sensor/adxl372/src/main.c
+++ samples/sensor/adxl372/src/main.c
@@ -10,6 +10,14 @@
 
 #define pow2(x) ((x) * (x))
 
+#define SENSOR_READINGS		50
+#define WAKEUP_THRESH 		0 /* (35 * SENSOR_G) */
+
+static struct sensor_trigger trig = {
+	.type = SENSOR_TRIG_DATA_READY,
+	.chan = SENSOR_CHAN_ACCEL_XYZ,
+};
+
 static double sqrt(double value)
 {
 	int i;
@@ -26,15 +34,53 @@ static double sqrt(double value)
 	return sqrt;
 }
 
+static int sensor_set_attribute(struct device *dev, enum sensor_channel chan,
+				enum sensor_attribute attr, int value)
+{
+	struct sensor_value sensor_val;
+	int ret;
+
+	if (attr == SENSOR_ATTR_SAMPLING_FREQUENCY) {
+		sensor_val.val1 = value;
+	} else {
+		sensor_val.val1 = value / 1000000;
+		sensor_val.val2 = value % 1000000;
+	}
+
+	ret = sensor_attr_set(dev, chan, attr, &sensor_val);
+	if (ret) {
+		printf("sensor_attr_set failed ret %d\n", ret);
+	}
+
+	return ret;
+}
+
 K_SEM_DEFINE(sem, 0, 1);
 
 static void trigger_handler(struct device *dev, struct sensor_trigger *trigger)
 {
+	static int cnt = SENSOR_READINGS;
 	ARG_UNUSED(trigger);
+
 
 	if (sensor_sample_fetch(dev)) {
 		printf("sensor_sample_fetch failed\n");
 		return;
+	}
+
+	if (WAKEUP_THRESH && (cnt-- == 0)) {
+		cnt = SENSOR_READINGS;
+		sensor_trigger_set(dev, &trig, NULL);
+
+		sensor_set_attribute(dev, SENSOR_CHAN_ACCEL_XYZ,
+				     SENSOR_ATTR_WAKEUP_THRESH, WAKEUP_THRESH);
+
+		printf("\n --- Waiting for threshold wakeup event ---\n");
+
+		if (sensor_trigger_set(dev, &trig, trigger_handler)) {
+			printf("Could not set trigger\n");
+
+		}
 	}
 
 	k_sem_give(&sem);
@@ -54,13 +100,14 @@ void main(void)
 		return;
 	}
 
-	struct sensor_trigger trig = {
-		.type = SENSOR_TRIG_DATA_READY,
-		.chan = SENSOR_CHAN_ACCEL_XYZ,
-	};
-
 	if (IS_ENABLED(CONFIG_ADXL372_PEAK_DETECT_MODE)) {
 		trig.type = SENSOR_TRIG_THRESHOLD;
+	}
+
+	if (WAKEUP_THRESH) {
+		sensor_set_attribute(dev, SENSOR_CHAN_ACCEL_XYZ,
+				SENSOR_ATTR_SAMPLING_FREQUENCY,
+				400);
 	}
 
 	if (IS_ENABLED(CONFIG_ADXL372_TRIGGER)) {
