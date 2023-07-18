a5b6a597beb2ceff9f8b6b9b7f1014c634a0e46d
Antoniu Miclaus <antoniu.miclaus@analog.com>
2023-07-18 18:28:42 +0300

    initial test setup
    Signed-off-by: Antoniu Miclaus <antoniu.miclaus@analog.com>

--- samples/hello_world/src/main.c
+++ samples/hello_world/src/main.c
@@ -1,13 +1,27 @@
+/* Networking DHCPv4 client */
+
 /*
- * Copyright (c) 2012-2014 Wind River Systems, Inc.
+ * Copyright (c) 2017 ARM Ltd.
+ * Copyright (c) 2016 Intel Corporation.
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 
 #include <zephyr/kernel.h>
+#include <zephyr/device.h>
+#include <zephyr/linker/sections.h>
+#include <errno.h>
+#include <stdio.h>
+
+#include <zephyr/net/net_if.h>
+#include <zephyr/net/net_core.h>
+#include <zephyr/net/net_context.h>
+#include <zephyr/net/net_mgmt.h>
+#include <zephyr/sys/__assert.h>
 
 int main(void)
 {
-	printk("Hello World! %s\n", CONFIG_BOARD);
+	const struct device *const dev = DEVICE_DT_GET_ONE(adi_adin1110);
+
 	return 0;
 }
