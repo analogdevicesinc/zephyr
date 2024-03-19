c1505d0a4f20f7bf966eeba635bfe2b5ca0752a6
Ciprian Regus <ciprian.regus@analog.com>
2024-03-19 11:04:49 +0200

    Add ADIN6310 driver
    Implement an ethernet driver for the ADIN6310 switch.
    
    Signed-off-by: Ciprian Regus <ciprian.regus@analog.com>

---
+++ drivers/ethernet/eth_adin6310.h
@@ -0,0 +1,64 @@
+#ifndef _ETH_ADIN6310_PRIV_H_
+#define _ETH_ADIN6310_PRIV_H_
+
+#include <zephyr/kernel.h>
+#include <stdio.h>
+#include <zephyr/device.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/drivers/spi.h>
+#include "zephyr/sys/util.h"
+#include "SES_port_api.h"
+
+#define ADIN6310_NUM_PORTS	6
+#define ADIN6310_SPI_WR_HEADER	0x40
+#define ADIN6310_SPI_RD_HEADER	0x80
+
+struct adin6310_data;
+
+enum adin6310_chip_id {
+	ADIN3310 = 0,
+	ADIN6310 = 1,
+};
+
+struct adin6310_port_config {
+	struct device *adin;
+	uint32_t id;
+	struct adin6310_data *net_device;
+	uint8_t mac_addr[6];
+	char *name;
+	bool ethernet_port;
+};
+
+struct adin6310_port_data {
+	struct device *adin;
+	uint32_t id;
+	struct adin6310_data *net_device;
+	struct net_if *iface;
+	uint8_t mac_addr[6];
+	char *name;
+	bool initialized;
+	bool ethernet_port;
+};
+
+struct adin6310_data {
+	const struct device *port[ADIN6310_NUM_PORTS];
+	struct adin6310_port_data *port_data[ADIN6310_NUM_PORTS];
+	struct gpio_callback gpio_int_callback;
+	const struct gpio_dt_spec *interrupt;
+	struct k_sem offload_sem;
+	struct k_mutex lock;
+	uint16_t ifaces_left_to_init;
+	uint8_t *buf;
+
+	struct k_thread rx_thread;
+};
+
+struct adin6310_config {
+	enum adin6310_chip_id id;
+	struct spi_dt_spec spi;
+	const struct gpio_dt_spec interrupt;
+	const struct gpio_dt_spec reset;
+	const struct adin6310_port_config port_config[ADIN6310_NUM_PORTS];
+};
+
+#endif /* _ETH_ADIN6310_PRIV_H_ */
