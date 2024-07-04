000dbb2a39cb7fe9d1feaa605cb3fd9135f58415
Ciprian Regus <ciprian.regus@analog.com>
2024-07-04 11:52:58 +0300

    remove unused

--- drivers/ethernet/eth_adin6310.h
+++ drivers/ethernet/eth_adin6310.h
@@ -45,7 +45,6 @@ struct adin6310_data {
 	struct adin6310_port_data *port_data[ADIN6310_NUM_PORTS];
 	struct gpio_callback gpio_int_callback;
 	const struct gpio_dt_spec *interrupt;
-	struct k_sem offload_sem;
 	struct k_mutex lock;
 	uint8_t frame_buf[CONFIG_ETH_ADIN6310_BUFFER_SIZE];
 	uint8_t rx_buf[CONFIG_ETH_ADIN6310_BUFFER_SIZE];
