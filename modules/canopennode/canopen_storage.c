6b54af017ead8d4838fbe8df0e68e5d067d62feb
Ioan Dragomir <ioan.dragomir@analog.com>
2025-03-31 16:26:31 +0300

    modules: canopennode: Merge github.com/NTUacingTeam/zephyr
    Bring NTURacingTeam implementation of CANOpenNode module, which ports v4.
    
    Signed-off-by: Ioan Dragomir <ioan.dragomir@analog.com>

--- modules/canopennode/canopen_storage.c
+++ modules/canopennode/canopen_storage.c
@@ -1,231 +1,170 @@
 /*
  * Copyright (c) 2019 Vestas Wind Systems A/S
+ * Copyright (c) 2024 National Taiwan University Racing Team
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdio.h>
+
+#include <zephyr/logging/log.h>
 #include <zephyr/settings/settings.h>
 
 #include <CANopen.h>
-#include <CO_Emergency.h>
-#include <CO_SDO.h>
+#include <storage/CO_storage.h>
+#include "OD.h"
 
 #include <canopennode.h>
 
-#define LOG_LEVEL CONFIG_CANOPEN_LOG_LEVEL
-#include <zephyr/logging/log.h>
-LOG_MODULE_REGISTER(canopen_storage);
+LOG_MODULE_REGISTER(canopen_storage, CONFIG_CANOPEN_LOG_LEVEL);
 
-/* 's', 'a', 'v', 'e' from LSB to MSB */
-#define STORE_PARAM_MAGIC   0x65766173UL
+#if CO_CONFIG_STORAGE & CO_CONFIG_STORAGE_ENABLE
 
-/* 'l', 'o', 'a', 'd' from LSB to MSB */
-#define RESTORE_PARAM_MAGIC 0x64616F6CUL
+static ODR_t store(CO_storage_entry_t *entry, CO_CANmodule_t *CANmodule);
 
-/* Variables for reporting errors through CANopen once the stack is up */
-static int canopen_storage_rom_error;
-static int canopen_storage_eeprom_error;
+static ODR_t restore(CO_storage_entry_t *entry, CO_CANmodule_t *CANmodule);
 
-static CO_SDO_abortCode_t canopen_odf_1010(CO_ODF_arg_t *odf_arg)
+static int canopen_settings_set(const char *key, size_t len_rd, settings_read_cb read_cb,
+				void *cb_arg);
+
+/**
+ * @brief Global pointer to the canopen instance since we are unable to retrieve it from settings
+ * handlers.
+ */
+static struct canopen *coo;
+static bool settings_set_err = false;
+
+SETTINGS_STATIC_HANDLER_DEFINE(canopen, CONFIG_CANOPENNODE_STORAGE_SUBTREE, NULL,
+			       canopen_settings_set, NULL, NULL);
+
+int canopen_storage_init(struct canopen *co)
 {
-	CO_EM_t *em = odf_arg->object;
-	uint32_t value;
 	int err;
+	CO_t *CO = co->CO;
+	OD_entry_t *OD_1010;
+	OD_entry_t *OD_1011;
 
-	value = CO_getUint32(odf_arg->data);
+	coo = co;
 
-	if (odf_arg->reading) {
-		return CO_SDO_AB_NONE;
+	co->storage.enabled = false;
+
+	OD_1010 = OD_find(OD, OD_H1010_STORE_PARAMETERS);
+	if (OD_1010 == NULL) {
+		LOG_ERR("object dictionary error at entry 0x1010");
+		return -EINVAL;
 	}
 
-	/* Preserve old value */
-	memcpy(odf_arg->data, odf_arg->ODdataStorage, sizeof(uint32_t));
-
-	if (odf_arg->subIndex != 1U) {
-		return CO_SDO_AB_NONE;
+	OD_1011 = OD_find(OD, OD_H1011_RESTORE_DEFAULT);
+	if (OD_1011 == NULL) {
+		LOG_ERR("object dictionary error at entry 0x1011");
+		return -EINVAL;
 	}
 
-	if (value != STORE_PARAM_MAGIC) {
-		return CO_SDO_AB_DATA_TRANSF;
+	err = CO_storage_init(&co->storage, CO->CANmodule, OD_1010, OD_1011, store, restore,
+			      co->storage_entries, co->storage_entries_count);
+	if (err != CO_ERROR_NO) {
+		LOG_ERR("CO_storage_init failed (err %d)", err);
+		return -EIO;
 	}
 
-	err = canopen_storage_save(CANOPEN_STORAGE_ROM);
-	if (err) {
-		LOG_ERR("failed to save object dictionary ROM entries (err %d)",
-			err);
-		CO_errorReport(em, CO_EM_NON_VOLATILE_MEMORY, CO_EMC_HARDWARE,
-			       err);
-		return CO_SDO_AB_HW;
-	} else {
-		LOG_DBG("saved object dictionary ROM entries");
+	err = settings_subsys_init();
+	if (err < 0) {
+		LOG_ERR("failed to initialize settings subsystem (err %d)", err);
+		return err;
 	}
 
-	return CO_SDO_AB_NONE;
+	err = settings_load_subtree(CONFIG_CANOPENNODE_STORAGE_SUBTREE);
+	if (err < 0) {
+		LOG_ERR("failed to load settings subtree (err %d)", err);
+		return err;
+	}
+
+	// settings_set_err is set if error occurs during settings loading
+	if (settings_set_err) {
+		LOG_ERR("failed to load some settings data");
+		return -EIO;
+	}
+
+	co->storage.enabled = true;
+
+	return 0;
 }
 
-static CO_SDO_abortCode_t canopen_odf_1011(CO_ODF_arg_t *odf_arg)
+int canopen_storage_process(struct canopen *co)
 {
-	CO_EM_t *em = odf_arg->object;
-	bool failed = false;
-	uint32_t value;
 	int err;
 
-	value = CO_getUint32(odf_arg->data);
-
-	if (odf_arg->reading) {
-		return CO_SDO_AB_NONE;
-	}
-
-	/* Preserve old value */
-	memcpy(odf_arg->data, odf_arg->ODdataStorage, sizeof(uint32_t));
-
-	if (odf_arg->subIndex < 1U) {
-		return CO_SDO_AB_NONE;
-	}
-
-	if (value != RESTORE_PARAM_MAGIC) {
-		return CO_SDO_AB_DATA_TRANSF;
-	}
-
-	err = canopen_storage_erase(CANOPEN_STORAGE_ROM);
-	if (err == -ENOENT) {
-		LOG_DBG("no object dictionary ROM entries to delete");
-	} else if (err) {
-		LOG_ERR("failed to delete object dictionary ROM entries"
-			" (err %d)", err);
-		CO_errorReport(em, CO_EM_NON_VOLATILE_MEMORY, CO_EMC_HARDWARE,
-			       err);
-		failed = true;
-	} else {
-		LOG_DBG("deleted object dictionary ROM entries");
-	}
-
-#ifdef CONFIG_CANOPENNODE_STORAGE_HANDLER_ERASES_EEPROM
-	err = canopen_storage_erase(CANOPEN_STORAGE_EEPROM);
-	if (err == -ENOENT) {
-		LOG_DBG("no object dictionary EEPROM entries to delete");
-	} else if (err) {
-		LOG_ERR("failed to delete object dictionary EEPROM entries"
-			" (err %d)", err);
-		CO_errorReport(em, CO_EM_NON_VOLATILE_MEMORY, CO_EMC_HARDWARE,
-			       err);
-		failed = true;
-	} else {
-		LOG_DBG("deleted object dictionary EEPROM entries");
-	}
-#endif
-
-	if (failed) {
-		return CO_SDO_AB_HW;
-	}
-
-	return CO_SDO_AB_NONE;
-}
-
-static int canopen_settings_set(const char *key, size_t len_rd,
-				settings_read_cb read_cb, void *cb_arg)
-{
-	const char *next;
-	int nlen;
-	ssize_t len;
-
-	nlen = settings_name_next(key, &next);
-
-	if (!strncmp(key, "eeprom", nlen)) {
-		struct sCO_OD_EEPROM eeprom;
-
-		len = read_cb(cb_arg, &eeprom, sizeof(eeprom));
-		if (len < 0) {
-			LOG_ERR("failed to restore object dictionary EEPROM"
-				" entries (err %zu)", len);
-			canopen_storage_eeprom_error = len;
-		} else {
-			if ((eeprom.FirstWord == CO_OD_FIRST_LAST_WORD) &&
-			    (eeprom.LastWord == CO_OD_FIRST_LAST_WORD)) {
-				memcpy(&CO_OD_EEPROM, &eeprom,
-				       sizeof(CO_OD_EEPROM));
-				LOG_DBG("restored object dictionary EEPROM"
-					" entries");
-			} else {
-				LOG_WRN("object dictionary EEPROM entries"
-					" signature mismatch, skipping"
-					" restore");
+	for (size_t i = 0; i < co->storage_entries_count; i++) {
+		if (co->storage_entries[i].attr & CO_storage_auto) {
+			err = store(&co->storage_entries[i], co->CO->CANmodule);
+			if (err != ODR_OK) {
+				return err;
 			}
 		}
-
-		return 0;
-	} else if (!strncmp(key, "rom", nlen)) {
-		struct sCO_OD_ROM rom;
-
-		len = read_cb(cb_arg, &rom, sizeof(rom));
-		if (len < 0) {
-			LOG_ERR("failed to restore object dictionary ROM"
-				" entries (err %zu)", len);
-			canopen_storage_rom_error = len;
-		} else {
-			if ((rom.FirstWord == CO_OD_FIRST_LAST_WORD) &&
-			    (rom.LastWord == CO_OD_FIRST_LAST_WORD)) {
-				memcpy(&CO_OD_ROM, &rom, sizeof(CO_OD_ROM));
-				LOG_DBG("restored object dictionary ROM"
-					" entries");
-			} else {
-				LOG_WRN("object dictionary ROM entries"
-					" signature mismatch, skipping"
-					" restore");
-			}
-		}
-
-		return 0;
 	}
 
 	return 0;
 }
 
-SETTINGS_STATIC_HANDLER_DEFINE(canopen, "canopen", NULL,
-			       canopen_settings_set, NULL, NULL);
-
-void canopen_storage_attach(CO_SDO_t *sdo, CO_EM_t *em)
+static ODR_t store(CO_storage_entry_t *entry, CO_CANmodule_t *CANmodule)
 {
-	CO_OD_configure(sdo, OD_H1010_STORE_PARAM_FUNC, canopen_odf_1010,
-			em, 0U, 0U);
-	CO_OD_configure(sdo, OD_H1011_REST_PARAM_FUNC, canopen_odf_1011,
-			em, 0U, 0U);
+	int err;
+	char key[100];
 
-	if (canopen_storage_eeprom_error) {
-		CO_errorReport(em, CO_EM_NON_VOLATILE_MEMORY, CO_EMC_HARDWARE,
-			       canopen_storage_eeprom_error);
+	ARG_UNUSED(CANmodule);
+
+	snprintf(key, sizeof(key), CONFIG_CANOPENNODE_STORAGE_SUBTREE "/%s", entry->key);
+	err = settings_save_one(key, entry->addr, entry->len);
+	if (err < 0) {
+		LOG_ERR("failed to save settings data %s (err %d)", entry->key, err);
+		return ODR_HW;
 	}
 
-	if (canopen_storage_rom_error) {
-		CO_errorReport(em, CO_EM_NON_VOLATILE_MEMORY, CO_EMC_HARDWARE,
-			       canopen_storage_rom_error);
-	}
+	return ODR_OK;
 }
 
-int canopen_storage_save(enum canopen_storage storage)
+static ODR_t restore(CO_storage_entry_t *entry, CO_CANmodule_t *CANmodule)
 {
-	int ret = 0;
+	int err;
+	char key[100];
 
-	if (storage == CANOPEN_STORAGE_ROM) {
-		ret = settings_save_one("canopen/rom", &CO_OD_ROM,
-					sizeof(CO_OD_ROM));
-	} else if (storage == CANOPEN_STORAGE_EEPROM) {
-		ret = settings_save_one("canopen/eeprom", &CO_OD_EEPROM,
-					sizeof(CO_OD_EEPROM));
+	ARG_UNUSED(CANmodule);
+
+	snprintf(key, sizeof(key), CONFIG_CANOPENNODE_STORAGE_SUBTREE "/%s", entry->key);
+	err = settings_delete(key);
+	if (err < 0) {
+		LOG_ERR("failed to delete settings data %s (err %d)", entry->key, err);
+		return ODR_HW;
 	}
 
-	return ret;
+	return ODR_OK;
 }
 
-int canopen_storage_erase(enum canopen_storage storage)
+static int canopen_settings_set(const char *key, size_t len_rd, settings_read_cb read_cb,
+				void *cb_arg)
 {
-	int ret = 0;
+	const char *next;
+	ssize_t len;
+	size_t i;
 
-	if (storage == CANOPEN_STORAGE_ROM) {
-		ret = settings_delete("canopen/rom");
-	} else if (storage == CANOPEN_STORAGE_EEPROM) {
-		ret = settings_delete("canopen/eeprom");
+	for (i = 0; i < coo->storage_entries_count; i++) {
+		if (settings_name_steq(key, coo->storage_entries[i].key, &next) && !next) {
+			len = read_cb(cb_arg, coo->storage_entries[i].addr,
+				      coo->storage_entries[i].len);
+			if (len < 0) {
+				LOG_ERR("failed to load settings data %s (err %d)",
+					coo->storage_entries[i].key, len);
+				settings_set_err = true;
+				return len;
+			}
+
+			return 0;
+		}
 	}
 
-	return ret;
+	return -ENOENT;
 }
+
+#endif /* CO_CONFIG_STORAGE */
